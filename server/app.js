8ba36970d5a9ed79376cd5d75f054c285663ed98
nithin <nithin@inncircles.com>
2023-05-15 19:33:36 +0530

    changes

--- server/app.js
+++ server/app.js
@@ -7,8 +7,14 @@ const { List, Task } = require('./db/models');
 
 app.use(bodyParser.json());
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get('/lists', (req, res) => {
-    List.find({}).then((lists) => {
+    List.find({ _userId: req.user_id }).then((lists) => {
         res.send(lists);
     }).catch((e) => {
         res.send(e);
